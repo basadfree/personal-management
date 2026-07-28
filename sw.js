ea9499a1314625ec36fe63ea0616a0163818659a
basadfree <binyamin.rot@gmail.com>
2026-07-28 18:56:00 +0300

    עדכון Service Worker (v2) לאלץ רענון מטמון

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-manager-v1';
+const CACHE = 'personal-manager-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -15,7 +15,7 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
 
